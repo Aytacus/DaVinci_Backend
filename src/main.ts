80fd1a431445c1483dea5e3a3a8e71a065c2873e
Aytacus <92356087+Aytacus@users.noreply.github.com>
2025-09-09 21:08:07 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://da-vinci-frontend.vercel.app' }); // frontend portuna izin ver
-  await app.listen(3001);
+  
+  app.enableCors({
+    origin: 'https://da-vinci-frontend.vercel.app', // sadece senin frontendin
+  });
+
+  const port = process.env.PORT || 3001; // Render buraya kendi portunu koyar
+  await app.listen(port);
 }
 bootstrap();
